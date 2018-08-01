fe1635ec08961059d4408bc0f1b972ce5cf27ed2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:15:34 +0300

    s

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,8 @@
-// Create your 'me' object literal here!
+var me = {
+	firstName:"Or",
+	lastName:" Tal",
+	favouriteFoods: ["pizza, ice cream, burger"],
+	age: 15,
+	picture: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwio-PjovMvcAhXoz4UKHXdsDRcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.visitlondon.com%2Ftraveller-information%2Fgetting-around-london%2Flondon-maps-and-guides%2Fwelcome-to-london&psig=AOvVaw3Gr3e6dY0CYSSEQhBthAVs&ust=1533199886601081",
+}
+document.write(JSON.stringify(me))
