6a5c948d7ff5d35904af51cb09c321686ca10815
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 17:23:26 +0300

    ss

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -2,6 +2,11 @@
 var pictures = ['./imgs/dog.jpg'];
 var currentIndex = 0;
 
+var firstpic = document.getElementsByTagName("img")[0];
+firstpic.addEventListener("click", function (event) {
+    alert("You clicked!");
+var pictures = ["./imgs/dog.jpg","buckpalace.jpeg","londoneye.jpeg","shard.jpeg","telephone.jpeg"]
+});
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
